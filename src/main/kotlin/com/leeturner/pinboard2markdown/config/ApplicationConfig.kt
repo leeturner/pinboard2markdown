a618b078e20023729882d009bbf9d19e5182989e
Lee Turner <lee@leeturner.me>
2020-05-10 22:14:18 +0100

    LT - no longer need the ObjectMapper @Bean and specify the timeout of the RestTemplate to be 5 seconds

--- src/main/kotlin/com/leeturner/pinboard2markdown/config/ApplicationConfig.kt
+++ src/main/kotlin/com/leeturner/pinboard2markdown/config/ApplicationConfig.kt
@@ -2,22 +2,20 @@ package com.leeturner.pinboard2markdown.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 import java.util.*
-
 
 @Configuration
 class ApplicationConfig {
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper().registerModule(KotlinModule())
-
-    @Bean
-    fun restTemplate(): RestTemplate {
+    fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate {
         // annoyingly the pinboard.in api return a content-type of "text/plain; charset=utf-8"
         // for their json responses.  This means we need to force the jackson message converter
         // to process the text/plain content type as well as the json one.
@@ -27,7 +25,9 @@ class ApplicationConfig {
         // not only application/*json, which is the default behaviour
         converter.supportedMediaTypes = listOf(MediaType.ALL)
         messageConverters.add(converter)
-        val restTemplate = RestTemplate()
+        val restTemplate = restTemplateBuilder
+            .setConnectTimeout(Duration.ofSeconds(5))
+            .build()
         restTemplate.messageConverters = messageConverters
         return restTemplate
     }
