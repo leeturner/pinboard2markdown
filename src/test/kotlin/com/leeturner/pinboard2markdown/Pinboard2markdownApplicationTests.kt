a85307f6442993a0cbc4e2b1cc0db3fc892eb1ff
Lee Turner <lee@leeturner.me>
2020-05-17 13:43:42 +0100

    LT - Updated the spring annotations so we can run this integration test without running the CommandlineRunner.

--- src/test/kotlin/com/leeturner/pinboard2markdown/Pinboard2markdownApplicationTests.kt
+++ src/test/kotlin/com/leeturner/pinboard2markdown/Pinboard2markdownApplicationTests.kt
@@ -1,9 +1,15 @@
 package com.leeturner.pinboard2markdown
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(classes = [Pinboard2markdownApplication::class, PropertySourcesPlaceholderConfigurer::class], initializers = [ConfigFileApplicationContextInitializer::class])
 class Pinboard2markdownApplicationTests {
 
     @Test
