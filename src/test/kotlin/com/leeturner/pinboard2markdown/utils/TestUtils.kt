28180173dfbc9245ac0630f79abbc40b4b70c22b
Lee Turner <lee@leeturner.me>
2020-05-31 18:36:29 +0100

    LT - more flexible pinboardResponse creation methods

--- src/test/kotlin/com/leeturner/pinboard2markdown/utils/TestUtils.kt
+++ src/test/kotlin/com/leeturner/pinboard2markdown/utils/TestUtils.kt
@@ -7,17 +7,8 @@ object TestUtils {
 
     const val NEWSLETTER_TAG = "newsletter"
 
-    fun createPinboardResponse(): PinboardResponse {
-        val post1 = Post("https://www.kaggle.com/",
-            "Kaggle: Your Home for Data Science",
-            "",
-            "bdac04c26a2e94ce95c3fe4f402564f4",
-            "e6ad936182532d34190def725f12e7a4",
-            "2018-11-10T16:50:02Z",
-            "no",
-            "no",
-            "ai machine-learning deeplearning4j $NEWSLETTER_TAG")
-        return PinboardResponse("2020-05-06T20:58:54Z", "username", listOf(post1))
+    fun createPinboardResponse(date: String = "", user: String = "", posts: List<Post> = emptyList()): PinboardResponse {
+        return PinboardResponse(date, user, posts)
     }
 
     fun createPost(desc: String = "Strikt",
@@ -27,9 +18,9 @@ object TestUtils {
         return Post(href = href,
             description = desc,
             extended = extended,
-            meta = "meta",
-            hash = "hash",
-            time = "time",
+            meta = "bdac04c26a2e94ce95c3fe4f402564f4",
+            hash = "e6ad936182532d34190def725f12e7a4",
+            time = "2018-11-10T16:50:02Z",
             shared = "no",
             toread = "no",
             tags = tags)
