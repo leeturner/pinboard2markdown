3d82e6cebc76b3256326a5920514b80690b58cf9
Lee Turner <lee@leeturner.me>
2020-05-31 18:34:15 +0100

    LT - change version number to first release

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.leeturner"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_13
 
 val developmentOnly by configurations.creating
